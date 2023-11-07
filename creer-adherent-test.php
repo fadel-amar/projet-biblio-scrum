e01634cf5d467de7bdbd64320bcf7305efdd2b4b
fadel-amar <fadel-amar@outlook.com>
2023-11-07 14:19:37 +0100

    mise en place shemaspy

--- creer-adherent-test.php
+++ creer-adherent-test.php
@@ -1,6 +1,8 @@
 <?php
+
 require 'bootstrap.php';
 require 'vendor/autoload.php';
+
 
 $adherant = new \App\Entity\Adherant();
 $adherant->setNumeroAdherant('AD-145614');
@@ -8,6 +10,7 @@ $adherant->setNom('Bob');
 $adherant->setPrenom('Sandra');
 $adherant->setEmail('sb@gmail.com');
 $adherant->setDateAdhesion(new DateTime());
+
 
 $entityManager->persist($adherant);
 $entityManager->flush();
